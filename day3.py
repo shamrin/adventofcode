e27a905be065c82bf4fdfc45a5a6e077ac811dd6
Alexey Shamrin <alexey.shamrin@futurice.com>
2021-12-03 14:15:44 +0200

    tinker

--- day3.py
+++ day3.py
@@ -5,19 +5,20 @@ def most_common(inp, i):
         >= sum(1 for n in inp if n[i] == '0')
 def least_common(inp, i):
     return not most_common(inp, i)
-def s(bit): return str(int(bit))
+def char(bit):
+    return str(int(bit))
 
 # part 1
 N = len(inp[0])
-g = ''.join(s(most_common(inp, i)) for i in range(N))
-e = ''.join(s(least_common(inp, i)) for i in range(N))
+g = ''.join(char(most_common(inp, i)) for i in range(N))
+e = ''.join(char(least_common(inp, i)) for i in range(N))
 print(int(g, 2) * int(e, 2))
 
 # part 2
 def distill(inp, bit):
     i = 0
     while len(inp) > 1:
-        inp = [n for n in inp if n[i] == s(bit(inp, i))]
+        inp = [n for n in inp if n[i] == char(bit(inp, i))]
         i += 1
     return int(inp[0], 2)
 print(distill(inp, most_common) * distill(inp, least_common))
