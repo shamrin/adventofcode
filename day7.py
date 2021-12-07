099d2baef41e80625b3f6101e68c1937ce64f855
Alexey Shamrin <alexey.shamrin@futurice.com>
2021-12-07 08:22:13 +0200

    slightly more readable

--- day7.py
+++ day7.py
@@ -1,9 +1,9 @@
 inp = open('day7.txt').read()
 d = list(map(int, inp.split(',')))
 
-def solve(f): return min(sum(f(c, p) for c in d) for p in range(min(d), max(d)+1))
-def cost1(c, p): return abs(c - p)
-def cost2(c, p): return abs(c - p) * (abs(c - p) + 1) // 2
+def solve(cost): return min(sum(cost(abs(c - p)) for c in d) for p in range(min(d), max(d)+1))
+def cost1(dist): return dist
+def cost2(dist): return (1 + dist) * dist // 2
 
 print(solve(cost1))
 print(solve(cost2))
