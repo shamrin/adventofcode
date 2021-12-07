e72eb8b86658e1b6446326cc789aa53dfcf051a5
Alexey Shamrin <alexey.shamrin@futurice.com>
2021-12-07 07:50:39 +0200

    optimise, inspired by https://github.com/vincentvanderweele/adventofcode/blob/main/2021/day07.js

--- day7.py
+++ day7.py
@@ -3,7 +3,7 @@ d = list(map(int, inp.split(',')))
 
 def solve(f): return min(sum(f(c, p) for c in d) for p in range(min(d), max(d)+1))
 def cost1(c, p): return abs(c - p)
-def cost2(c, p): return sum(i for i in range(1, abs(c - p) + 1))
+def cost2(c, p): return abs(c - p) * (abs(c - p) + 1) // 2
 
 print(solve(cost1))
 print(solve(cost2))
