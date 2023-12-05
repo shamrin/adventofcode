a58fb3f167fe43f1143f857c7aa93146763a7e5e
Alexey Shamrin <shamrin@gmail.com>
2023-12-06 00:56:22 +0200

    clean (Map struct)

--- 2023/5/5.go
+++ 2023/5/5.go
@@ -27,6 +27,10 @@ func ints[T Integer](text string) []T {
 }
 
 type R struct{ start, end uint32 }
+type Map struct {
+	dst uint32
+	src R
+}
 
 func intersection(a, b R) []R {
 	start := max(a.start, b.start)
@@ -55,32 +59,31 @@ func difference(a, b R) []R {
 	return r
 }
 
-func part1(M [][][]uint32, seeds []uint32) uint32 {
+func part1(sections [][]Map, seeds []uint32) uint32 {
 	x := slices.Clone(seeds)
 	nw := 4
-	for _, section := range M {
+	for _, maps := range sections {
 		var wg sync.WaitGroup
 		for w := 0; w < nw; w++ {
 			wg.Add(1)
-			go func(w int, section [][]uint32) {
+			go func(w int, maps []Map) {
 				defer wg.Done()
 				for i := w; i < len(x); i += nw {
-					for _, m := range section {
-						dst, src, length := m[0], m[1], m[2]
-						if src <= x[i] && x[i] < src+length {
-							x[i] += dst - src
+					for _, m := range maps {
+						if m.src.start <= x[i] && x[i] < m.src.end {
+							x[i] += m.dst - m.src.start
 							break
 						}
 					}
 				}
-			}(w, section)
+			}(w, maps)
 		}
 		wg.Wait()
 	}
 	return slices.Min(x)
 }
 
-func part2Brute(maps [][][]uint32, specs []uint32) uint32 {
+func part2Brute(sections [][]Map, specs []uint32) uint32 {
 	sum := uint32(0)
 	for i := 0; i < len(specs)-1; i += 2 {
 		sum += specs[i+1]
@@ -91,26 +94,27 @@ func part2Brute(maps [][][]uint32, specs []uint32) uint32 {
 			seeds = append(seeds, s)
 		}
 	}
-	return part1(maps, seeds)
+	return part1(sections, seeds)
 }
 
-func part2(maps [][][]uint32, specs []uint32) uint32 {
+func part2(sections [][]Map, specs []uint32) uint32 {
 	rs := []R{}
 	for i := 0; i < len(specs)-1; i += 2 {
 		rs = append(rs, R{specs[i], specs[i] + specs[i+1]})
 	}
-	for _, section := range maps {
+	for _, maps := range sections {
 		next := make([]R, 0)
 		for len(rs) > 0 {
 			handled := false
-			var r R
-			r, rs = rs[0], rs[1:]
-			for _, m := range section {
-				dst, src, length := m[0], m[1], m[2]
-				srcR := R{src, src + length}
-				if inter := intersection(r, srcR); len(inter) > 0 {
-					next = append(next, R{inter[0].start + dst - src, inter[0].end + dst - src})
-					rs = append(rs, difference(r, srcR)...)
+			r := rs[0]
+			rs = rs[1:]
+			for _, m := range maps {
+				if inter := intersection(r, m.src); len(inter) > 0 {
+					next = append(next, R{
+						start: inter[0].start + m.dst - m.src.start,
+						end:   inter[0].end + m.dst - m.src.start,
+					})
+					rs = append(rs, difference(r, m.src)...)
 					handled = true
 					break
 				}
@@ -139,16 +143,18 @@ func main() {
 	lines := strings.Split(input, "\n\n")
 	specs := ints[uint32](lines[0])
 
-	maps := [][][]uint32{}
+	sections := [][]Map{}
 	for i, section := range lines[1:] {
 		ranges := strings.Split(section, "\n")
-		maps = append(maps, [][]uint32{})
-		for _, range_ := range ranges[1:] {
-			maps[i] = append(maps[i], ints[uint32](range_))
+		sections = append(sections, []Map{})
+		for _, r := range ranges[1:] {
+			m := ints[uint32](r)
+			dst, src, length := m[0], m[1], m[2]
+			sections[i] = append(sections[i], Map{dst, R{src, src + length}})
 		}
 	}
 
-	fmt.Println(part1(maps, specs))
-	fmt.Println(part2(maps, specs))
-	// fmt.Println(part2Brute(maps, specs))
+	fmt.Println(part1(sections, specs))
+	fmt.Println(part2(sections, specs))
+	// fmt.Println(part2Brute(sections, specs))
 }
