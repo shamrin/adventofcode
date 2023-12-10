118a7654baab2a341109e9faf8f61635f53f17a7
Alexey Shamrin <shamrin@gmail.com>
2023-12-10 19:54:14 +0200

    clean up and generalize

--- 2023/10/10.go
+++ 2023/10/10.go
@@ -17,16 +17,6 @@ var boxchars = map[rune]rune{
 	'S': 'S',
 }
 
-var boldchars = map[rune]rune{
-	'│': '┃',
-	'─': '━',
-	'└': '┗',
-	'┘': '┛',
-	'┐': '┓',
-	'┌': '┏',
-	'S': '┏', // hardcoded
-}
-
 type xy struct {
 	x int
 	y int
@@ -47,9 +37,7 @@ func size(field map[xy]rune) (nrows, ncols int) {
 
 func print(field map[xy]rune) {
 	nrows, ncols := size(field)
-	fmt.Println("    01234567890")
 	for y := 0; y < nrows; y++ {
-		fmt.Printf("%3d ", y)
 		for x := 0; x < ncols; x++ {
 			fmt.Print(string(field[xy{x, y}]))
 		}
@@ -64,20 +52,13 @@ func main() {
 	}
 	input := string(file)
 
-	pairs := []string{}
-	for from, to := range boxchars {
-		pairs = append(pairs, string(from), string(to))
-	}
-	r := strings.NewReplacer(pairs...)
-	input = r.Replace(input)
-
 	lines := strings.Split(input, "\n")
 	field := map[xy]rune{}
 	start := xy{}
 	for y, line := range lines {
 		x := 0
 		for _, c := range line {
-			field[xy{x, y}] = c
+			field[xy{x, y}] = boxchars[c]
 			if c == 'S' {
 				start = xy{x, y}
 			}
@@ -85,17 +66,35 @@ func main() {
 		}
 	}
 
+	// set start element
+	west := strings.ContainsRune("─└┌", field[xy{start.x - 1, start.y}])
+	east := strings.ContainsRune("┘┐─", field[xy{start.x + 1, start.y}])
+	north := strings.ContainsRune("│┐┌", field[xy{start.x, start.y - 1}])
+	south := strings.ContainsRune("│┘└", field[xy{start.x, start.y + 1}])
+	switch {
+	case west && north:
+		field[start] = '─'
+	case west && east:
+		field[start] = '┘'
+	case west && south:
+		field[start] = '┐'
+	case north && east:
+		field[start] = '└'
+	case north && south:
+		field[start] = '│'
+	case east && south:
+		field[start] = '┌'
+	}
+
+	// walk the loop (part 1)
 	steps := 0
 	loop := map[xy]bool{}
-	prev := start
-	loop[start] = true
-	x, y := start.x+1, start.y // hardcoded
-	for (xy{x, y} != start) {
+	prev := xy{-2, -2}
+	x, y := start.x, start.y
+	for {
 		pos := xy{x, y}
 		loop[pos] = true
 		switch field[pos] {
-		case 'S':
-			panic("oops")
 		case '│':
 			if y-1 == prev.y {
 				y++
@@ -132,39 +131,41 @@ func main() {
 			} else {
 				y++
 			}
-		default:
-			panic("oops")
+		}
+		if start.x == x && start.y == y {
+			break
 		}
 		steps++
 		prev = pos
 	}
 	fmt.Println(steps/2 + 1)
 
+	// erase all non-loop elements
+	for pos := range field {
+		if !loop[pos] {
+			field[pos] = '.'
+		}
+	}
+	// print(field)
+
+	// count inner elements (part 2)
 	nrows, ncols := size(field)
 	ins := 0
 	for y := 0; y < nrows; y++ {
 		topIn, bottomIn := false, false
 		for x := 0; x < ncols; x++ {
-			c := field[xy{x, y}]
-			switch {
-			case !loop[xy{x, y}]:
+			switch field[xy{x, y}] {
+			case '.':
 				if topIn {
 					ins++
 				}
-			case c == '│':
+			case '│':
 				topIn = !topIn
 				bottomIn = !bottomIn
-			case c == '─':
-			case c == '└':
+			case '└', '┘':
 				topIn = !topIn
-			case c == '┘':
-				topIn = !topIn
-			case c == '┐':
+			case '┐', '┌':
 				bottomIn = !bottomIn
-			case c == '┌' || c == 'S': // hardcoded
-				bottomIn = !bottomIn
-			default:
-				panic("oops")
 			}
 		}
 	}
