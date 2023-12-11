67bd8cd42b4504c699cd7b193a7086cee2d0ca83
Alexey Shamrin <shamrin@gmail.com>
2023-12-11 08:57:08 +0200

    clean more

--- 2023/11/11.py
+++ 2023/11/11.py
@@ -3,20 +3,20 @@ from itertools import combinations
 inp = open('11.txt').read()
 k = 10**6
 
-gs = {(r, c) for r, line in enumerate(inp.split("\n")) for c, cell in enumerate(line) if cell == '#'}
+gs = {(r, c) for r, L in enumerate(inp.split("\n")) for c, ch in enumerate(L) if ch == '#'}
 
-nr = len(inp.split("\n"))
-nc = len(inp.split("\n")[0])
+nr = max(r for r, _ in gs) + 1
+nc = max(c for _, c in gs) + 1
 
-grows = set(r for r, _ in gs)
-gcols = set(c for _, c in gs)
+grows = {r for r, _ in gs}
+gcols = {c for _, c in gs}
 
 emptyrows = sorted(r for r in range(nr) if r not in grows)
 emptycols = sorted(c for c in range(nc) if c not in gcols)
 
 for er in reversed(emptyrows):
-    gs = set((r+k-1 if r > er else r, c) for r, c in gs)
+    gs = {(r+k-1 if r > er else r, c) for r, c in gs}
 for ec in reversed(emptycols):
-    gs = set((r, c+k-1 if c > ec else c) for r, c in gs)
+    gs = {(r, c+k-1 if c > ec else c) for r, c in gs}
 
 print(sum(abs(r1 - r2) + abs(c1 - c2) for (r1, c1), (r2, c2) in combinations(gs, 2)))
